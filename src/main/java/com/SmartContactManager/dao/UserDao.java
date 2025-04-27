4d8c391616358712da9c6a68c3210e6c92117cd2
goswamirahu <girirahul5152@gmail.com>
2025-04-27 18:18:27 +0530

    Update UserDao.java

--- src/main/java/com/SmartContactManager/dao/UserDao.java
+++ src/main/java/com/SmartContactManager/dao/UserDao.java
@@ -1,2 +1,14 @@
-package com.SmartContactManager.dao;public class UserDao {
+package com.SmartContactManager.dao;
+
+import com.SmartContactManager.Entity.User;
+
+import java.util.List;
+
+public interface UserDao {
+    Long saveUser(User user);
+    User findByEmailAndPassword(String email, String password);
+    List<User> getAllUsers();
+    User getUserById(long id);
+    void updateUser(User user);
+    void deleteUser(long id);
 }
